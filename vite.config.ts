529ad3ac3291259a025cd65c7d5675f2f757fb20
Mahmoud Abdelwahab <m@mahmoudw.com>
2025-03-24 12:53:06 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    host: "0.0.0.0",
+    host: true,
   },
 });
