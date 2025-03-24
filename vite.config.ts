f20750a2926518760b9f96716deab6eb5f765c10
Mahmoud Abdelwahab <hey@mahmoud.codes>
2025-03-24 14:45:11 +0100

    remove server configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT,
-  },
 });
